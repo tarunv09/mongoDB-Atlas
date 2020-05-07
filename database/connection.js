22f20e8a7031718a822319458d83747354bbf5b7
Tarun Varshney <imtarunvarshney@gmail.com>
2020-05-08 03:31:10 +0530

    First Commit

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,16 @@
 
 const mongoose = require("mongoose");
 
-const uri =  "mongodb+srv://user:<password>@<cluster-name>-kpump.mongodb.net/test?retryWrites=true&w=majority";
+
+/*<user> is your atlas database username, 
+  <password> is your atlas database password,
+  <cluster-name> is your atlas database cluster.
+ *I'm directly using some credentials here for ease of demonstration. 
+ *However, this is not the recommended way to use credentials.
+ *Try to use dotenv node module and store credentials in .env file.
+*/
+
+const uri =  "mongodb+srv://<user>:<password>@<cluster-name>-kpump.mongodb.net/test?retryWrites=true&w=majority";
 
 const connectDB = async() => {
     await mongoose.connect(uri), {useUnifiedTopology: true, useNewUrlParser: true};
